2c02dfb8349223c208080e30494dc3231e48ba3a
jucanuro <juankanunezr@gmail.com>
2024-07-03 16:45:03 -0500

    nuevo diseño vicaf

--- static/js/vicaf.js
+++ static/js/vicaf.js
@@ -1,46 +1,18 @@
-  // JavaScript para manejar la apertura y cierre del modal
-  document.getElementById('openModalBtn').addEventListener('click', function() {
-    document.getElementById('myModal').classList.remove('hidden');
-});
+const menuToggle = document.getElementById('menu-toggle');
+        const mobileMenu = document.getElementById('mobile-menu');
 
-document.getElementById('closeModalBtn').addEventListener('click', function() {
-    document.getElementById('myModal').classList.add('hidden');
-});
+        menuToggle.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
 
-// Cerrar el modal cuando se hace clic fuera del contenido del modal
-window.addEventListener('click', function(event) {
-    const modal = document.getElementById('myModal');
-    if (event.target === modal) {
-        modal.classList.add('hidden');
-    }
-});
-let elements = document.querySelectorAll("[data-menu]");
-for (let i = 0; i < elements.length; i++) {
-    let main = elements[i];
+        const track = document.querySelector('.carousel-track');
+        const images = document.querySelectorAll('.carousel-image');
+        let currentIndex = 0;
 
-    main.addEventListener("click", function () {
-    let element = main.parentElement.parentElement;
-    let indicators = main.querySelectorAll("img");
-    let child = element.querySelector("#menu");
-    let h = element.querySelector("#mainHeading>div>p");
+        function nextSlide() {
+            currentIndex = (currentIndex + 1) % images.length;
+            const offset = -currentIndex * 100;
+            track.style.transform = `translateX(${offset}%)`;
+        }
 
-    h.classList.toggle("font-semibold");
-    child.classList.toggle("hidden");
-    // console.log(indicators[0]);
-    indicators[0].classList.toggle("rotate-180");
-    });
-};
-const menuItems = document.querySelectorAll('.menu-item');
-menuItems.forEach(item => {
-    item.addEventListener('click', function () {
-        menuItems.forEach(el => el.classList.remove('active'));
-        this.classList.add('active');
-    });
-});
-
-// Toggle mobile menu visibility
-const mobileMenuButton = document.getElementById('mobile-menu-button');
-const mobileMenu = document.getElementById('mobile-menu');
-mobileMenuButton.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-});
+        setInterval(nextSlide, 5000);
