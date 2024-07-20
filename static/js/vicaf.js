4cae7ae0b278e61afcf5735da1c681c3852e3e27
jucanuro <juankanunezr@gmail.com>
2024-07-20 13:42:18 -0500

    Actualizacion 20-07-2024

--- static/js/vicaf.js
+++ static/js/vicaf.js
@@ -16,3 +16,61 @@ const menuToggle = document.getElementById('menu-toggle');
         }
 
         setInterval(nextSlide, 5000);
+
+        function selectTab(tabId) {
+            // Quitar la clase 'selected' de todas las pestañas
+            document.querySelectorAll('.tab').forEach(tab => {
+                tab.classList.remove('bg-blue-500', 'text-white');
+                tab.classList.add('bg-gray-200', 'text-gray-700');
+            });
+
+            // Añadir la clase 'selected' a la pestaña seleccionada
+            const selectedTab = document.getElementById(tabId);
+            selectedTab.classList.remove('bg-gray-200', 'text-gray-700');
+            selectedTab.classList.add('bg-blue-500', 'text-white');
+        }
+        
+        document.addEventListener('DOMContentLoaded', function () {
+            const items = document.querySelectorAll('.carousel-item');
+            let currentIndex = 0;
+
+            function showNextItem() {
+                items[currentIndex].classList.remove('active');
+                currentIndex = (currentIndex + 1) % items.length;
+                items[currentIndex].classList.add('active');
+            }
+
+            setInterval(showNextItem, 3000);
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            const modalButtons = document.querySelectorAll('[data-modal]');
+            const modals = document.querySelectorAll('.modal');
+            const modalCloseButtons = document.querySelectorAll('.modal-close');
+
+            modalButtons.forEach(button => {
+                button.addEventListener('click', function () {
+                    const modalId = this.getAttribute('data-modal');
+                    document.getElementById(modalId).classList.add('active');
+                });
+            });
+
+            modalCloseButtons.forEach(button => {
+                button.addEventListener('click', function () {
+                    this.closest('.modal').classList.remove('active');
+                });
+            });
+
+            modals.forEach(modal => {
+                modal.addEventListener('click', function (e) {
+                    if (e.target === modal) {
+                        modal.classList.remove('active');
+                    }
+                });
+            });
+        });
+    
+    
+
+
+    
